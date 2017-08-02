02a3af412353693a7aecbdb508562cd6efb78a39
David Mallard <correspondence@davidmallard.id.au>
2017-08-02 17:07:59 +1000

    Add a basic model with data for 6 locations

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,37 @@ function initMap() {
 		zoom: 13
 	});
 }
+
+// Locations array provides the model component of the app
+var Locations = [
+	{
+		name: 'The Hotel Canobolas',
+		address: '248 Summer St',
+		location: { lat: -33.2844163, lng: 149.1018213 }
+	},
+	{
+		name: 'Parkview Hotel Orange',
+		address: '281 Summer St',
+		location: { lat: -33.2836838, lng: 149.1020493 }
+	},
+	{
+		name: 'Eighteen 70',
+		address: '85 March St',
+		location: { lat: -33.2783207 , lng: 149.0922561 }
+	},
+	{
+		name: 'Sweet Sour Salt',
+		address: '116 Summer St',
+		location: { lat: -33.283295, lng: 149.0936483 }
+	},
+	{
+		name: 'Bills Beans East Orange',
+		address: '148 McLachlan St',
+		location: { lat: -33.2849981, lng: 149.1060653 }
+	},
+	{
+		name: 'Byng Street Cafe & Local Store',
+		address: '47 Byng St',
+		location: { lat: -33.2800973, lng: 149.0893966 }
+	}
+];
