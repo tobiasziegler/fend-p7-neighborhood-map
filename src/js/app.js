752f956acaddab70a39b9220c981d9e7223625e8
David Mallard <correspondence@davidmallard.id.au>
2017-09-17 15:24:45 +1000

    Add Foursquare API requests and display data in the InfoWindow

--- src/js/app.js
+++ src/js/app.js
@@ -35,17 +35,54 @@ function initMap() {
 		});
 
 		location.marker = marker;
+
+		requestAPIData(location);
 	});
 
 	ko.applyBindings(vm);
 }
 
+// Request third-party API data to add to a location
+function requestAPIData(location) {
+	var foursquareUrl = 'https://api.foursquare.com/v2/venues/' +
+			location.foursquareID + '?client_id=' + config.FOURSQUARE_CLIENT_ID +
+			'&client_secret=' + config.FOURSQUARE_CLIENT_SECRET + '&v=20170916';
+
+	// Request venue information from the Foursquare API and add received data
+	// to the location
+	$.getJSON(foursquareUrl)
+	.done(function (data) {
+		if (data.response.venue.url) {
+			location.website = data.response.venue.url;
+		}
+		if (data.response.venue.hours) {
+			location.hours = data.response.venue.hours.status;
+		}
+	})
+	.fail(function() {
+		console.log('Foursquare API data for ' + location.name + ' could not be loaded.');
+	});
+}
+
 // Assemble the content to display in the InfoWindow
 function getInfoWindowContent(location) {
-	var nameDiv = '<div>' + location.name + '<div>';
-	var addressDiv = '<div>' + location.address + '</div>';
+	var infoWindowContent = '<h3>' + location.name + '</h3>';
+	infoWindowContent += '<div>' + location.address + '</div>';
 
-	return nameDiv + addressDiv;
+	// Add any details that have been retrieved from the Foursquare API
+	if (location.website) {
+		infoWindowContent += '<div><a target="_blank" href="' + location.website + '"> ' + location.website + '</a></div>';
+	} else {
+		infoWindowContent += '<div>Website URL is unknown</div>';
+	}
+
+	if (location.hours) {
+		infoWindowContent += '<div>' + location.hours + '</div>';
+	} else {
+		infoWindowContent += '<div>Opening hours are unknown</div>';
+	}
+
+	return infoWindowContent;
 }
 
 // Locations array provides the model component of the app
@@ -53,32 +90,38 @@ var Locations = [
 	{
 		name: 'The Hotel Canobolas',
 		address: '248 Summer St',
-		position: { lat: -33.2844163, lng: 149.1018213 }
+		position: { lat: -33.2844163, lng: 149.1018213 },
+		foursquareID: '4b63fdfbf964a520579a2ae3'
 	},
 	{
 		name: 'Parkview Hotel Orange',
 		address: '281 Summer St',
-		position: { lat: -33.2836838, lng: 149.1020493 }
+		position: { lat: -33.2836838, lng: 149.1020493 },
+		foursquareID: '4cb817997148f04de519cdab'
 	},
 	{
 		name: 'Eighteen 70',
 		address: '85 March St',
-		position: { lat: -33.2783207 , lng: 149.0922561 }
+		position: { lat: -33.2783207 , lng: 149.0922561 },
+		foursquareID: '4e71d11baeb79ea095196fc1'
 	},
 	{
 		name: 'Sweet Sour Salt',
 		address: '116 Summer St',
-		position: { lat: -33.283295, lng: 149.0936483 }
+		position: { lat: -33.283295, lng: 149.0936483 },
+		foursquareID: '4ca2abf8b0ff8cfa6e19b15e'
 	},
 	{
 		name: 'Bills Beans East Orange',
 		address: '148 McLachlan St',
-		position: { lat: -33.2849981, lng: 149.1060653 }
+		position: { lat: -33.2849981, lng: 149.1060653 },
+		foursquareID: '4c3283daa0ced13a4053156e'
 	},
 	{
 		name: 'Byng Street Cafe & Local Store',
 		address: '47 Byng St',
-		position: { lat: -33.2800973, lng: 149.0893966 }
+		position: { lat: -33.2800973, lng: 149.0893966 },
+		foursquareID: '4ea2d5c07ee52a35dd0d7a32'
 	}
 ];
 
