f226fa9e0502719631b8b8b9a5d0899b81ef0515
David Mallard <correspondence@davidmallard.id.au>
2017-09-21 19:05:20 +1000

    Separate the JSON data into its own file

---
+++ src/js/data.js
@@ -0,0 +1,39 @@
+// Locations array provides the model component of the app
+var locations = [
+	{
+		name: 'The Hotel Canobolas',
+		address: '248 Summer St',
+		position: { lat: -33.2844163, lng: 149.1018213 },
+		foursquareID: '4b63fdfbf964a520579a2ae3'
+	},
+	{
+		name: 'Parkview Hotel Orange',
+		address: '281 Summer St',
+		position: { lat: -33.2836838, lng: 149.1020493 },
+		foursquareID: '4cb817997148f04de519cdab'
+	},
+	{
+		name: 'Eighteen 70',
+		address: '85 March St',
+		position: { lat: -33.2783207 , lng: 149.0922561 },
+		foursquareID: '4e71d11baeb79ea095196fc1'
+	},
+	{
+		name: 'Sweet Sour Salt',
+		address: '116 Summer St',
+		position: { lat: -33.283295, lng: 149.0936483 },
+		foursquareID: '4ca2abf8b0ff8cfa6e19b15e'
+	},
+	{
+		name: 'Bills Beans East Orange',
+		address: '148 McLachlan St',
+		position: { lat: -33.2849981, lng: 149.1060653 },
+		foursquareID: '4c3283daa0ced13a4053156e'
+	},
+	{
+		name: 'Byng Street Cafe & Local Store',
+		address: '47 Byng St',
+		position: { lat: -33.2800973, lng: 149.0893966 },
+		foursquareID: '4ea2d5c07ee52a35dd0d7a32'
+	}
+];
