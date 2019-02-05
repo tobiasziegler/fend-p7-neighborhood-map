b947023a4e4990ca3b6f9dce2df4abf391023f07
David Mallard <correspondence@davidmallard.id.au>
2019-02-05 14:59:14 +1100

    Rebuild the app

--- dist/js/sidebar.js
+++
@@ -1,1 +0,0 @@
-var btn=document.getElementById("sidebar-button"),sidebar=document.getElementById("sidebar");btn.addEventListener("click",function(t){sidebar.classList.toggle("open"),btn.classList.toggle("open"),t.stopPropagation()});
